ad499e2c11dfae0d8cdf4a5ba020b3795bb4a9ee
Siddanth Karia <siddanthkaria7@gmail.com>
2023-10-15 16:26:33 -0700

    create a game coin filp

--- src/assets/script/javascript.js
+++ src/assets/script/javascript.js
@@ -6,5 +6,12 @@ document.getElementById("close").onclick = function(){
   modal.style.display = 'none';
 }
 
+var modal = document.getElementById('createmodal');
+document.getElementById("openModal").onclick = function(){
+  modal.style.display = 'block';
+}
+document.getElementById("close").onclick = function(){
+  modal.style.display = 'none';
+}
 
 
