8cdb3a5997ed16245cceb644733a9c6a8029c309
Siddanth Karia <siddanthkaria7@gmail.com>
2023-10-15 11:03:55 -0700

    java work

---
+++ src/assets/script/javascript.js
@@ -0,0 +1,10 @@
+var modal = document.getElementById('modal');
+document.getElementById("openModal").onclick = function(){
+  modal.style.display = 'block';
+}
+document.getElementById("close").onclick = function(){
+  modal.style.display = 'none';
+}
+
+
+
